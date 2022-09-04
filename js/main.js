f23f72764d743cc0db597dde6786c1f599a79250
wuyawei <wyw@wuyaweideMacBook-Pro.local>
2022-09-04 14:16:03 +0800

    implement a renderer

--- js/main.js
+++ js/main.js
@@ -8,5 +8,24 @@ const app = {
     },
     children: 'click me'
 };
-function render(vnode) {
+function render(vnode, root) {
+    const el = document.createElement(vnode.tag);
+    Object.keys(vnode.props).forEach(key => {
+        if (key.startsWith('on')) {
+            el.addEventListener(key.substring(2).toLocaleLowerCase(), vnode.props[key]);
+        }
+    });
+    if (typeof vnode.children === 'string') {
+        el.innerText = vnode.children;
+    }
+    else {
+        for (let child of vnode.children) {
+            render(child, el);
+        }
+    }
+    root.appendChild(el);
+}
+const root = document.querySelector('#app');
+if (root) {
+    render(app, root);
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,8 +1,10 @@
 type VNode = {
-  
+  tag: string
+  props: Record<string, any>
+  children: VNode[] | string
 }
 
-const app = {
+const app: VNode = {
   tag: 'div',
   props: {
     onClick() {
@@ -12,6 +14,25 @@ const app = {
   children: 'click me'
 }
 
-function render(vnode) {
-  
+function render(vnode: VNode, root: Element) {
+  const el = document.createElement(vnode.tag)
+  Object.keys(vnode.props).forEach(key => {
+    if (key.startsWith('on')) {
+      el.addEventListener(key.substring(2).toLocaleLowerCase(), vnode.props[key])
+    }
+  })
+  if (typeof vnode.children === 'string') {
+    el.innerText = vnode.children
+  } else {
+    for (let child of vnode.children) {
+      render(child, el)
+    }
+  }
+
+  root.appendChild(el)
+}
+
+const root = document.querySelector('#app')
+if (root) {
+  render(app, root)
 }
